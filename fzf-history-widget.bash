ec4269edae29ef9c4913c04758bb7391e2d8fbe3
Paulo Vitor Vieira <paulo.vieira@byne.com.br>
2025-10-31 11:26:59 -0300

    fix: reset TTY for fzf ssh history widget

--- fzf-history-widget.bash
+++ fzf-history-widget.bash
@@ -1,23 +1,24 @@
 __fzf_history_widget() {
-  local selected
+  stty sane 2>/dev/null
+  local selected key command
   selected=$(history | awk '{ lines[NR] = $0 } END { for (i = NR; i > 0; i--) print lines[i] }' \
     | fzf --exact --tiebreak=index --no-sort --reverse --height=40% \
       --bind 'ctrl-r:toggle-sort' --expect=ctrl-e \
       --query="$READLINE_LINE")
-
-  local key command
   key=$(head -n1 <<< "$selected")
   command=$(tail -n1 <<< "$selected" | sed -E 's/ *[0-9]+\*? //')
-
   if [[ -n "$command" ]]; then
     READLINE_LINE="$command"
     READLINE_POINT=${#READLINE_LINE}
     if [[ "$key" != "ctrl-e" ]]; then
       echo "$READLINE_LINE"
+      stty sane 2>/dev/null
       eval "$READLINE_LINE"
       READLINE_LINE=""
       READLINE_POINT=0
+      stty sane 2>/dev/null
     fi
   fi
 }
+
 bind -x '"\C-r": __fzf_history_widget'
