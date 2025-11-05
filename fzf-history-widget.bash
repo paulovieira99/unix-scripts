5a62fff7613b897fb3633fb24d2ef65ab200e35b
Paulo Vitor Vieira <paulo.vieira@byne.com.br>
2025-11-05 13:26:39 -0300

    fix: properly insert FZF history selection into command line buffer

--- fzf-history-widget.bash
+++ fzf-history-widget.bash
@@ -1,39 +1,24 @@
-# Histórico gigante
-HISTSIZE=100000
-HISTFILESIZE=100000
-HISTFILE=~/.bash_history
-
-# Opções avançadas
-shopt -s histappend       # não sobrescrever o arquivo
-shopt -s cmdhist          # agrupa multiline
-shopt -s lithist          # salva com formatação literal
-
-# Evitar duplicados
+export HISTSIZE=10000
+export HISTFILESIZE=20000
 export HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
 
-# Salva imediatamente
-PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+if [ -f ~/.fzf.bash ]; then
+    source ~/.fzf.bash
+elif [ -f /usr/share/fzf/key-bindings.bash ]; then
+    source /usr/share/fzf/key-bindings.bash
+elif [ -f /usr/share/doc/fzf/examples/key-bindings.bash ]; then
+    source /usr/share/doc/fzf/examples/key-bindings.bash
+elif [ -f /opt/homebrew/opt/fzf/shell/key-bindings.bash ]; then
+    # Para macOS com Homebrew
+    source /opt/homebrew/opt/fzf/shell/key-bindings.bash
+fi
 
-# Timestamp nos comandos
-export HISTTIMEFORMAT='%F %T '
-
-fzf_history() {
-  local selected
-  selected=$(history | sed -E 's/^[[:space:]]*[0-9]+[[:space:]]+([0-9]{4}-[0-9]{2}-[0-9]{2}[[:space:]][0-9]{2}:[0-9]{2}:[0-9]{2}[[:space:]]+)?//' \
-    | awk 'NF' \
-    | sort -u \
-    | fzf --height=50% --layout=reverse --border \
-          --prompt="history> " \
-          --preview 'echo {}' \
-          --preview-window=up:3:wrap \
-          --header 'ENTER: inserir na linha | ESC: cancelar' )
-
-  if [[ -n "$selected" ]]; then
-    READLINE_LINE="$selected"
-    READLINE_POINT=${#selected}
-  fi
-}
-
-# bind Ctrl+R
-bind -x '"\C-r": fzf_history'
-
+# Customiza as opções do FZF
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}'
+  --preview-window down:3:wrap
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --header 'Enter: inserir | Ctrl-Y: copiar'
+  --border
+  --height 40%"
